98e542003537f29748f6e2da10296ac14f6d72a4
Paulius101 <p.dambrausko@gmail.com>
2025-01-27 21:07:50 +0200

    code refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,28 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         ArrayList<Integer> resultList = new ArrayList<>();
+        ArrayList<Integer> dataList = getDataList();
+        Random random = new Random();
+        int stepSize = random.nextInt(1, 4);
         int startIndex = 0;
-        int endIndex = 3;
+        int endIndex = stepSize;
 
-        int numberOfThreads = 3;
+        int numberOfThreads = dataList.size() / stepSize;
+        int dataListSize = dataList.size();
         for (int i = 0; i < numberOfThreads; i++) {
             Thread thread = new Thread(new Worker(getDataList().subList(startIndex, endIndex), resultList));
             thread.start();
             thread.join();
-            startIndex += 3;
-            endIndex += 3;
+            startIndex += stepSize;
+            endIndex += stepSize;
+            if (endIndex > dataListSize) endIndex = dataListSize;
+            if (startIndex + stepSize > dataListSize - stepSize) startIndex = dataListSize - stepSize;
         }
 
         System.out.println(resultList);
