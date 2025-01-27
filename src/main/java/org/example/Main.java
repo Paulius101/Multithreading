9869208fc6268363723b577363ba30b022417050
Paulius101 <p.dambrausko@gmail.com>
2025-01-27 19:36:42 +0200

    task initial implementation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(5);
+        arrayList.add(4);
+        arrayList.add(8);
+        arrayList.add(6);
+        arrayList.add(1);
+        arrayList.add(10);
+        arrayList.add(9);
+        ArrayList<Integer> resultList = new ArrayList<>();
+
+
+        Worker worker1 = new Worker(arrayList.subList(0, 3), resultList);
+        Worker worker2 = new Worker(arrayList.subList(3, 6), resultList);
+        Worker worker3 = new Worker(arrayList.subList(3, arrayList.size()), resultList);
+
+        Thread thread1 = new Thread(worker1);
+        Thread thread2 = new Thread(worker2);
+        Thread thread3 = new Thread(worker3);
+
+        thread1.run();
+        thread2.run();
+        thread3.run();
+
+        System.out.println(worker1.getResultArray().toString());
+
     }
 }
---
+++ src/main/java/org/example/Worker.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.List;
+
+public class Worker implements Runnable {
+    private List<Integer> numberArray;
+    private List<Integer> resultArray;
+
+    public Worker(List<Integer> numberArray, List<Integer> resultArray) {
+        this.numberArray = numberArray;
+        this.resultArray = resultArray;
+    }
+
+    public synchronized void toSquare() {
+        for (int i = 0; i < numberArray.size(); i++) {
+            int number = numberArray.get(i);
+            numberArray.set(i, number * number);
+        }
+    }
+
+    @Override
+    public void run() {
+        toSquare();
+        resultArray.addAll(numberArray);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "resultArray=" + resultArray +
+                '}';
+    }
+
+    public List<Integer> getResultArray() {
+        return resultArray;
+    }
+}
