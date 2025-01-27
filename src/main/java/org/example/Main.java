817a7dd6cd6fbd77b09c5028b2c2cc1ecb1f7b90
Paulius101 <p.dambrausko@gmail.com>
2025-01-27 20:46:13 +0200

    code refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,27 @@
 package org.example;
+
 import java.util.ArrayList;
 
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+        ArrayList<Integer> resultList = new ArrayList<>();
+        int startIndex = 0;
+        int endIndex = 3;
+
+        int numberOfThreads = 3;
+        for (int i = 0; i < numberOfThreads; i++) {
+            Thread thread = new Thread(new Worker(getDataList().subList(startIndex, endIndex), resultList));
+            thread.start();
+            thread.join();
+            startIndex += 3;
+            endIndex += 3;
+        }
+
+        System.out.println(resultList);
+    }
+
+    private static ArrayList<Integer> getDataList() {
         ArrayList<Integer> arrayList = new ArrayList<>();
         arrayList.add(2);
         arrayList.add(3);
@@ -14,27 +32,6 @@ public class Main {
         arrayList.add(1);
         arrayList.add(10);
         arrayList.add(9);
-        ArrayList<Integer> resultList = new ArrayList<>();
-
-
-        Worker worker1 = new Worker(arrayList.subList(0, 3), resultList);
-        Worker worker2 = new Worker(arrayList.subList(3, 6), resultList);
-        Worker worker3 = new Worker(arrayList.subList(6, arrayList.size()), resultList);
-
-        Thread thread1 = new Thread(worker1);
-        Thread thread2 = new Thread(worker2);
-        Thread thread3 = new Thread(worker3);
-
-        thread1.start();
-        thread1.join();
-
-        thread2.start();
-        thread2.join();
-
-        thread3.start();
-        thread3.join();
-
-        System.out.println(resultList);
-
+        return arrayList;
     }
 }
